486cbc1c81305347f47c598960253f935bca7bce
Cezary Piatek <cezarypiatek@live.com>
2025-04-29 20:25:57 +0200

    Add the standalone component as an embedded resource, as the unpkg cdn seems to be unreliable.

--- FridaysForks.AsyncApi/AspNet/AsyncApiUIMiddleware.cs
+++ FridaysForks.AsyncApi/AspNet/AsyncApiUIMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
 
@@ -8,26 +9,50 @@ public class AsyncApiUIMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly string _htmlTemplate;
+    private readonly string _scriptContent;
+    private readonly string _stylesheetContent;
 
     public AsyncApiUIMiddleware(RequestDelegate next)
     {
         _next = next;
         var assembly = typeof(AsyncApiUIMiddleware).Assembly;
-        using var stream = assembly.GetManifestResourceStream("FridaysForks.AsyncApi.UI.asyncapi-ui.html");
+        _htmlTemplate = LoadEmbeddedContent(assembly, "asyncapi-ui.html");
+        _scriptContent = LoadEmbeddedContent(assembly, "index.js");
+        _stylesheetContent = LoadEmbeddedContent(assembly, "default.min.css");
+        
+    }
+
+    private static string LoadEmbeddedContent(Assembly assembly, string filename)
+    {
+        using var stream = assembly.GetManifestResourceStream($"FridaysForks.AsyncApi.UI.{filename}");;
         if (stream == null)
-            throw new InvalidOperationException("Embedded resource 'asyncapi-ui.html' not found.");
+            throw new InvalidOperationException($"Embedded resource '{filename}' not found.");
 
         using var reader = new StreamReader(stream);
-        _htmlTemplate = reader.ReadToEnd();
+        var content = reader.ReadToEnd();
+        return content;
     }
 
     public async Task InvokeAsync(HttpContext context, IAsyncApiRegistry registry)
     {
         var path = context.Request.Path.Value;
-        var match = Regex.Match(path, @"^/asyncapi/?$");
-
+        var match = Regex.Match(path, @"^/asyncapi/?(.*?)$");
         if (match.Success)
         {
+            if(match.Groups[1].Value == "index.js")
+            {
+                context.Response.ContentType = "application/javascript";
+                await context.Response.WriteAsync(_scriptContent);
+                return;
+            }
+            
+            if(match.Groups[1].Value == "default.min.css")
+            {
+                context.Response.ContentType = "text/css";
+                await context.Response.WriteAsync(_stylesheetContent);
+                return;
+            }
+            
             var allNames = await registry.GetAvailableNames();
             var allDocs = allNames.Select(x => new
             {
